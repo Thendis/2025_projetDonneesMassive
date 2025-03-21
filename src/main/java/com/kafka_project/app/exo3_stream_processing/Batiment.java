600ec3e2b4b4f37fb1270b15cc2e3901a2f6055f
Eddin MKAVAVO <em963948@MI115-02.iem>
2025-03-21 09:07:40 +0100

    stream processing ok

---
+++ src/main/java/com/kafka_project/app/exo3_stream_processing/Batiment.java
@@ -0,0 +1,59 @@
+package com.kafka_project.app.exo3_stream_processing;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class Batiment implements Runnable, Serializable {
+
+    private String nomBat;
+    private int nb_salle = 5;
+    Random rd = new Random();
+    Producer<String, String> producer;
+
+    public Batiment(String nomBAt_) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        
+        producer = new KafkaProducer<>(props);
+
+        nomBat = nomBAt_;
+        
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            LocalDateTime tsp = LocalDateTime.now();
+            final String sep = ";";
+            //Récupération des températures par salle
+            for (int salle_id = 0;salle_id<nb_salle;salle_id++){
+                String to_send = "salle"+salle_id+sep+LocalDateTime.now()+sep+rd.nextDouble(0, 35);
+                System.out.println("From "+nomBat+" -> "+to_send);
+                producer.send(new ProducerRecord<>("temperatures", nomBat, to_send));
+            }  
+
+            //Attente avant prochaine collecte
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+            }
+        }
+    }
+
+   @Override 
+    public String toString(){
+        String ret = "\t"+nomBat;
+        return ret;
+    } 
+
+}
---
+++ src/main/java/com/kafka_project/app/exo3_stream_processing/Thread_prod.java
@@ -0,0 +1,25 @@
+package com.kafka_project.app.exo3_stream_processing;
+
+import java.util.HashMap;
+
+public class Thread_prod {
+
+    public static void main(String[] args) {
+        final int nb_bat = 2;
+
+        HashMap<String, Thread> bats = new HashMap<>();
+        for (int bat_num = 0; bat_num < nb_bat; bat_num++) {
+            bats.put("bat" + bat_num, new Thread(new Batiment("bat" + bat_num)));
+        }
+
+        bats.forEach((k,v) -> {  // Start every bats
+            v.start();
+        });
+
+        bats.forEach((k,v) -> {try {  // Wait for every bats
+            v.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }});
+    }
+}
